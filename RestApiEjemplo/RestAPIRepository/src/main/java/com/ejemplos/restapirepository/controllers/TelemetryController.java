6a524f6e2eb650e7fe55dce77550cfdc3266564e
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-05-19 18:15:16 -0300

    url referencias

--- RestApiEjemplo/RestAPIRepository/src/main/java/com/ejemplos/restapirepository/controllers/TelemetryController.java
+++ RestApiEjemplo/RestAPIRepository/src/main/java/com/ejemplos/restapirepository/controllers/TelemetryController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ejemplos.restapirepository.dto.TelemetryDTO;
 
-
+//https://docs.spring.io/spring-data/jpa/reference/repositories/custom-implementations.html
 @RestController()
 @RequestMapping(value = "/telemetry")
 public class TelemetryController 
